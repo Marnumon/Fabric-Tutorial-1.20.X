dac89a690ac64077e278e7a8eb97de75021d0ac2
Marnumon <3518409917@qq.com>
2024-03-06 14:38:03 +0800

    Initial commit

---
+++ src/main/java/net/equinox/tutorialmod/TutorialMod.java
@@ -0,0 +1,22 @@
+package net.equinox.tutorialmod;
+
+import net.equinox.tutorialmod.block.ModBlocks;
+import net.equinox.tutorialmod.item.ModItemGroups;
+import net.equinox.tutorialmod.item.ModItems;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
+
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
+	}
+}
