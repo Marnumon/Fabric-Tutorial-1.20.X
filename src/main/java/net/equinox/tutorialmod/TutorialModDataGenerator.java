bc5d527e2e6cc7d9bb0122f27b4722bc63f940cc
Marnumon <3518409917@qq.com>
2024-03-08 15:31:16 +0800

    Backup

--- src/main/java/net/equinox/tutorialmod/TutorialModDataGenerator.java
+++ src/main/java/net/equinox/tutorialmod/TutorialModDataGenerator.java
@@ -1,11 +1,18 @@
 package net.equinox.tutorialmod;
 
+import net.equinox.tutorialmod.datagen.*;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class TutorialModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModBlockProvider::new);
+		pack.addProvider(ModItemProvider::new);
+		pack.addProvider(ModLootTableProvider::new);
+		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipeProvider::new);
 	}
 }
